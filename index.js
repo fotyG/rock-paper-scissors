30d64d413df96b7117a03e342ce8de30da5ab1f5
Sota Giorgadze <giorgadze@inbox.lv>
2023-05-22 11:48:45 +0300

    finished basic ui

--- index.js
+++ index.js
@@ -1,51 +1,79 @@
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const gameBoard = document.getElementById("gameboard");
+const playerS = document.getElementById("playerScore");
+const computerS = document.getElementById("computerScore");
+
+rockButton.addEventListener("click", getChoice);
+paperButton.addEventListener("click", getChoice);
+scissorsButton.addEventListener("click", getChoice);
+
 const getComputerChoice = () => {
   const choice = ["Rock", "Paper", "Scissors"];
-  return choice[Math.floor(Math.random() * choice.length)]
-}
+  return choice[Math.floor(Math.random() * choice.length)];
+};
 
 const playRound = (playerSelection, computerSelection) => {
-  if(playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-    return "It's a draw"
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    return "It's a draw";
   }
-  if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
-    return "You lose! Paper beats Rock"
+  if (
+    playerSelection.toLowerCase() === "rock" &&
+    computerSelection.toLowerCase() === "paper"
+  ) {
+    return "You lose! Paper beats Rock";
   }
-  if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
-    return "You win! Rock beats Scissors"
+  if (
+    playerSelection.toLowerCase() === "rock" &&
+    computerSelection.toLowerCase() === "scissors"
+  ) {
+    return "You win! Rock beats Scissors";
   }
-  if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-    return "You win! Paper beats Rock"
+  if (
+    playerSelection.toLowerCase() === "paper" &&
+    computerSelection.toLowerCase() === "rock"
+  ) {
+    return "You win! Paper beats Rock";
   }
-  if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
-    return "You lose! Scissors beat Paper"
+  if (
+    playerSelection.toLowerCase() === "paper" &&
+    computerSelection.toLowerCase() === "scissors"
+  ) {
+    return "You lose! Scissors beat Paper";
   }
-  if(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
-    return "You lose! Rock beats Scissors"
+  if (
+    playerSelection.toLowerCase() === "scissors" &&
+    computerSelection.toLowerCase() === "rock"
+  ) {
+    return "You lose! Rock beats Scissors";
   }
-  if(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
-    return "You win! Scissors beat Paper"
-  } else {
-    return "You can only choose: Rock, Paper or Scissors!"
+  if (
+    playerSelection.toLowerCase() === "scissors" &&
+    computerSelection.toLowerCase() === "paper"
+  ) {
+    return "You win! Scissors beat Paper";
   }
+  else {
+    return "Whoa.. Not so fast - computer can't keep up!🚀"
+  }
+};
+
+let playerScore = 0;
+let computerScore = 0;
+
+function getChoice(e) {
+  let playerChoice = e.target.attributes["data-value"].value;
+  return game(playerChoice);
 }
 
-const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
+function game(playerSelection) {
+  const computerSelection = getComputerChoice();
+  let result = playRound(playerSelection, computerSelection);
+  if (result?.split(" ")[1] === "win!") playerScore++;
+  if (result?.split(" ")[1] === "lose!") computerScore++;
+  gameBoard.innerHTML = result;
+  playerS.innerHTML = "Player: " + playerScore;
+  computerS.innerHTML = "Computer: " + computerScore;
+};
 
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("What's your weapon of choice?")
-    const computerSelection = getComputerChoice();
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result);
-    if (result.split(" ")[1] === "win!") playerScore++
-    if (result.split(" ")[1] === "lose!") computerScore++
-    
-    if (i === 4) {
-      if (playerScore === computerScore) return "It's a draw"
-      return playerScore > computerScore ? "Player wins" : "Computer wins"
-    }
-  }
-}
-
-console.log(game())
